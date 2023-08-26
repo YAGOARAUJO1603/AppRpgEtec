2471e24ba62c4f33423334c2831e655264ac42c4
YAGO1603 <y.desenvovedor1603@gmail.com>
2023-08-25 22:47:07 -0300

    Primeiro projeto

---
+++ Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppRpgEtec.Models
+{
+    internal class Usuario
+    {
+        public int Id { get; set; }
+        public String Username { get; set; }
+        public String PasswordString { get; set; }
+        public String Perfil { get; set; }
+        public String Token { get; set; }
+        public byte[] Foto { get; set; }
+        public String Email { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+    }
+}
---
+++ ViewModels/Usuarios/UsuarioViewModel.cs
@@ -0,0 +1,151 @@
+﻿using AppRpgEtec.Models;
+using AppRpgEtec.Services.Usuarios;
+using AppRpgEtec.Views.Usuarios;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AppRpgEtec.ViewModels.Usuarios
+{
+    public class UsuarioViewModel : BaseViewModel
+    {
+        private UsuarioService uService;
+        public ICommand RegistrarCommand { get; set; }
+        public ICommand AutenticarCommand { get; set; }
+        public ICommand DirecionarCadastroCommand { get; set; }
+
+
+        public UsuarioViewModel()
+        {
+            uService = new UsuarioService();
+            InicializarCommands();
+        }
+
+        public void InicializarCommands()
+        {
+            RegistrarCommand = new Command(async () => await RegistrarUsuario());
+            AutenticarCommand = new Command(async ()=>  await AutenticarUsuario());
+            DirecionarCadastroCommand = new Command(async ()=> await DirecionarParaCadastro());
+        }
+
+
+        private string login = string.Empty;
+        public string Login
+        {
+            get { return login; }
+            set {
+                login = value;
+                OnPropertyChanged(); }
+        }
+
+        private string senha = string.Empty;
+        public string Senha
+        {
+            get { return senha; }
+            set 
+            {
+            senha = value;
+            OnPropertyChanged();
+            }
+        }
+
+       public async Task RegistrarUsuario()
+        {
+            try
+            {
+                Usuario u = new Usuario();
+                u.Username = Login;
+                u.PasswordString = Senha;
+
+                Usuario uRegistrado = await uService.PostRegistrarUsuarioAsync(u);
+
+                if (uRegistrado != null)
+                {
+                    string mensagem = $"Usuario Id {uRegistrado.Id} registrado com sucesso. ";
+                    await Application.Current.MainPage.DisplayAlert("Informação ", mensagem, "Ok");
+
+                    await Application.Current.MainPage
+                        .Navigation.PopAsync(); //Remove a página da pilha de visualização
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+                await Application.Current.MainPage
+                    .DisplayAlert("Informação", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+
+        public async Task AutenticarUsuario() //Metodo para autenticar usuario
+        {
+            try
+            {
+                Usuario u = new Usuario();
+                u.Username = Login;
+                u.PasswordString = Senha;
+
+                Usuario uAutenticado = await uService.PostRegistrarUsuarioAsync(u);
+
+                if (!string.IsNullOrEmpty(uAutenticado.Token))
+                {
+                    string mensagem = $"Bem-vindo(a) {uAutenticado.Username}.";
+                    //Guardando dados do usuario para uso futuro
+                    Preferences.Set("UsuarioId", uAutenticado.Id);
+                    Preferences.Set("UsuarioUsername", uAutenticado.Username);
+                    Preferences.Set("UsuarioPerfil", uAutenticado.Perfil);
+                    Preferences.Set("UsuarioToken", uAutenticado.Token);
+
+                    await Application.Current.MainPage
+                        .DisplayAlert("Informação", mensagem, "Ok");
+
+                    Application.Current.MainPage = new MainPage();
+                }
+                else
+                {
+                    await Application.Current.MainPage
+                        .DisplayAlert("Informação ", "Dados incorretos :(", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                await Application.Current.MainPage
+                    .DisplayAlert("Informação", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+
+        public async Task DirecionarParaCadastro() //Metodo para exibir view de cadastro
+        {
+            try
+            {
+                await Application.Current.MainPage.
+                    Navigation.PushAsync(new CadastroView());
+            }
+            catch (Exception ex)
+            {
+
+                await Application.Current.MainPage
+                    .DisplayAlert("Informação", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
